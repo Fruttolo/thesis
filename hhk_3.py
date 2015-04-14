4c558d6ead65039cc5c7196e5df1ff56e9ec212e
Luca Casini <lucacasini.foligno@gmail.com>
2015-04-14 22:40:43 +0200

    revision to newhhk.py
    added efficient versions

--- hhk_3.py
+++ hhk_3.py
@@ -11,10 +11,9 @@ def naif(document):
     # initialize simulators
     sim = {node: tree.findall('.//' + node.tag) for node in nodes}
     # computer forward simulation
-    sim = forward_simulation(nodes, sim)
+    return forward_simulation(nodes, sim)
     # TODO: forward simulation
-    print_sim(nodes, sim)
-    return sim
+    #print_sim(nodes, sim)
 
 
 def forward_simulation(nodes, sim):
@@ -22,22 +21,23 @@ def forward_simulation(nodes, sim):
     c = 0
     # for each node u verify Forward Simulation
     flag = True
+
     while flag:
-        print c
+        #print "while: " + str(c)
         flag = False
         for u in nodes:
-            print counter
             # v = post(u)
             for v in u:
                 # w = sim[v]
                 if u is not None:
                     for w in sim[u]:
+                        #print "inner for "  + str(counter)
                         # intersection between post(w) and sim(v)
                         if len([val for val in w if val in sim[v]]) == 0:
                             flag = True
                             # remove w from the simulator of u
                             sim[u].remove(w)
-                            counter += 1
+                        counter += 1
         c += 1
     return sim
 
@@ -48,9 +48,11 @@ def efficient(document):
     # get the root node
     root = tree.getroot()
     # get all nodes in the tree
-    nodes = {node: 1 for node in root.iter()}
+    nodes = {root: 0}
+    l = list(root.iterdescendants())
+    nodes.update(dict(zip(l, range(1, len(l) + 1))))
     # initialize sets
-    sim = {}
+    sim = {root: root}
     # renamed prevsim(v) to oldsim to avoid confusion
     oldsim = {}
     remove = {}
@@ -58,11 +60,12 @@ def efficient(document):
     # counters for debugging
     c1 = 0
     c2 = 0
-    print "starting operations"
+    ##print "starting operations"
     # initialize sim(v), prevsim(v) and count(w'',u)
     # total cost is O(n^2) + O(mn) + O(mn) = O(mn) with m>n
     for node in nodes:
-        print "initializing node " + str(c1)
+
+        ##print "initializing node " + str(c1)
         # prevsim(v) is assigned in O(1)
         oldsim[node] = nodes
         # sim(v) is calculated in O(n^2)
@@ -72,52 +75,52 @@ def efficient(document):
             sim[node] = {n: nodes[n] for n in nodes if n.tag == node.tag and len(n) != 0}
 
         # remove vector is calculated in O(mn)
-        print "initializing remove"
-        remove[node] = pre(oldsim[node],nodes)
-        #print remove[node]
-        #print "remove of node " + node.text.strip()
+        ##print "initializing remove"
+        remove[node] = pre(oldsim[node], nodes)
+        # print remove[node]
+        # print "remove of node " + node.text.strip()
         for n in pre(sim[node], nodes):
             try:
                 del remove[node][n]
-                print "removed node " + n.text.strip()
+                #print "removed node " + n.text.strip()
             except KeyError:
                 pass
         if len(remove[node]) == 0:
             del remove[node]
-        print "remove done"
+        ##print "remove done"
 
         c1 += 1
-        print "node done"
-    #printnodes(oldsim)
-    #print "\nsim: "
+        ##print "node done"
+    # printnodes(oldsim)
+    # print "\nsim: "
     #printnodes(sim)
     #print "\nremove: "
     #printnodes(remove)
 
     # implement 2D array as a tuple-indexed dictionary
     # count is calculated in O(mn)
-    print "initializing count(x,u)"
+    ##print "initializing count(x,u)"
     for node in nodes:
         count.update(getcount(node, sim))
-    print "count done"
+    ##print "count done"
 
     # F-Similarity Check
     # we store keys in remove only if remove(v)!=0, so when all remove(v) = 0 our dictionary is empty
     while len(remove) != 0:
         print len(remove)
-        print "while cycle " + str(c2)
+        ##print "while cycle " + str(c2)
         # print "length of remove " + str(len(remove))
         c3 = 0
         keys = remove.keys()
         for node in keys:
-            print "internal for cycle : " + str(c3) #+ " NODE = " + node.text.strip()
+            ##print "internal for cycle : " + str(c3)  #+ " NODE = " + node.text.strip()
             #print node
             if node != root:
                 for u in [node.getparent()]:
                     print u.text
                     for w in remove[node]:
                         if w in sim[u]:
-                            print "deleting " + w.text.strip() + " from " + u.text.strip()
+                            #print "deleting " + w.text.strip() + " from " + u.text.strip()
                             del sim[u][w]
                             # decrement count
                             # count[(w.getparent(), u)] -= 1
@@ -126,22 +129,22 @@ def efficient(document):
                                 # remove[u] could not exist
                                 if count[(x, u)] == 0:
                                     if u in remove:
-                                        remove[u].update({x: 1})
+                                        remove[u].update({x: nodes[x]})
                                     else:
-                                        remove[u] = {x: 1}
+                                        remove[u] = {x: nodes[x]}
             oldsim[node] = sim[node]
             del remove[node]
             #print len(remove)
             c3 += 1
         c2 += 1
-    #print_sim(nodes, sim)
+    ##print_sim(nodes, sim)
     return sim
 
 
 def getcount(node, sim):
     # count(w'',u) =  post(w'') intersection sim(u)
     # we computer count(node, u) for all u given node
-    c = 0
+    c = []
     count = {}
     # for every simulator set with
     for u in sim:
@@ -150,9 +153,13 @@ def getcount(node, sim):
             count[(node, u)] = 0
         else:
             for p in node:
-                if sim[u].get(p):
-                    c += 1
-            count[(node, u)] = c
+                try:
+                    x = sim[u][p]
+                    c.append(x)
+                except KeyError:
+                    # print "getcountkeyerror"
+                    pass
+            count[(node, u)] = len(c)
     return count
 
 
@@ -161,14 +168,9 @@ def pre(nodelist, nodes):
     for n in nodelist:
         x = n.getparent()
         if x is not None:
-            res[x] = 1
+            res[x] = nodes[x]
+    # res = {n.getparent(): nodelist[n] for n in nodelist if n.getparent() is not None}
     return res
-
-
-def printnodes(nodes):
-    for node in nodes:
-        for n in nodes[node]:
-            print node.text.strip() + ": " + n.text.strip()
 
 
 def print_sim(nodes, sim):
@@ -178,4 +180,4 @@ def print_sim(nodes, sim):
             print y.text.strip() + '\n'
 
 
-efficient('ciao.xml')
+naif('ciao.xml')
--- newhhk.py
+++ newhhk.py
@@ -19,6 +19,14 @@ class graph:
                 p.append(e[1])
         return set(p)
 
+    def pre(self,node_index):
+        p = []
+        for n in node_index:
+            for e in self.edges:
+                if e[1]== n:
+                    p.append(e[0])
+        return set(p)
+
     def __str__(self):
         print str(self.edges) + "\n"
         for n in self.nodes:
@@ -40,7 +48,7 @@ def construct_tree(document):
     nodes = [n for n in root.iter()]
     c0 = 0
     for i,n in enumerate(nodes):
-        print c0
+        #print c0
         g.add_node(node(i, n.tag))
         try:
             p = nodes.index(n.getparent())
@@ -57,31 +65,76 @@ def construct_tree(document):
         c0 += 1
     return g
 
-                    #TODO: test for emptyness
 
 def naif(graph):
     sim = []
     for i,n in enumerate(graph.nodes):
         sim.insert(i,set([m.num for m in graph.nodes if m.tag == n.tag]))
     flag = True
-    #c = 0
-    #print "begin while"
+    c = 0
+    print "begin while"
     while flag:
-        #print c
-        #c += 1
+        print c
+        c += 1
         flag = False
         for u in graph.nodes:
             for v in graph.post(u.num):
                 rem = set([])
                 for w in sim[u.num]:
                     if len(graph.post(w).intersection(sim[v]))==0:
-                        #print str(u.num) +" "+ str(v) +" "+ str(w) + " removed"
+                        print str(u.num) +" "+ str(v) +" "+ str(w) + " removed"
                         #sim[u.num].remove(w)
                         rem.add(w)
                         flag = True
-                    #else:
-                         #print str(u.num) +" "+ str(v) +" "+ str(w)
+                    else:
+                         print str(u.num) +" "+ str(v) +" "+ str(w)
                 sim[u.num] = sim[u.num].difference(rem)
     return sim
 
-naif(construct_tree('/home/luca/tesi/ciao.xml'))
+
+def efficient(graph):
+    sim = []
+    prevsim = []
+    remove = []
+    print "starting"
+    for i, n in enumerate(graph.nodes):
+        prevsim.insert(i, [t.num for t in graph.nodes])
+        if len(graph.post(n))==0:
+            sim.insert(i, set([m.num for m in graph.nodes if m.tag == n.tag]))
+        else:
+            sim.insert(i, set([m.num for m in graph.nodes if m.tag == n.tag and len(graph.post(n)) != 0]))
+        remove.insert(i, graph.pre(prevsim[i]).difference(graph.pre(sim[i])))
+    # compute count(w'',u)
+    print "counting"
+    count = []
+    for n in graph.nodes:
+        count.insert(n.num,[])
+        for m in graph.nodes:
+            c = len(graph.post(n.num).intersection(sim[m.num]))
+            count[n.num].insert(m.num,c)
+    #print count
+    flag = True
+    while flag:
+        print "while"
+        flag = False
+        for v in graph.nodes:
+            print "for v"
+            for u in graph.pre([v.num]):
+                print "for u " + str(u)
+                for w in remove[v.num]:
+                    print "for w"
+                    if w in sim[u]:
+                        print "IF"
+                        sim[u].remove(w)
+                        for x in graph.pre([w]):
+                            print x
+                            if count[x][u] == 0:
+                                print "removing"
+                                remove[u].add(x)
+                                flag = True
+            prevsim[v.num] = sim[v.num]
+            remove[v.num].clear()
+    print sim
+    return sim
+
+efficient(construct_tree('./ciao.xml'))
